4a2a29b38d1544edcdd06ee91dd47dec762edd5c
Susnata Dutta <susnata.dutta@indusnet.co.in>
2023-10-17 12:08:19 +0530

    get the pathname by /app structure

--- app/news/[slug]/page.js
+++ app/news/[slug]/page.js
@@ -1,6 +1,13 @@
 //URL :: domain/news/important-news
+"use client";
+
+import { usePathname } from "next/navigation";
 
 const ImportantNewsPage = () => {
+  const path = usePathname();
+
+  console.log(path);
+
   return <h1 className="news-text">ImportantNewsPage</h1>;
 };
 
